e91eed07116fa38e859e601dcbec29233fafcdc4
atharva0608 <124123099+atharva0608@users.noreply.github.com>
2025-05-01 02:57:42 +0530

    Update owasp_dependency.groovy

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,4 +1,4 @@
-def call (){
-  dependencyCheck additionalArguments: '--scan ./',odcInstallation: 'OWASP'
-  depndencyCheckPublisher pattern: '++/dependency-checkreport.xml'
+def call(){
+  dependencyCheck additionalArguments: '--scan ./', odcInstallation: 'OWASP'
+  dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
 }
